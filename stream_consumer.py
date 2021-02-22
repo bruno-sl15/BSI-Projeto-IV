fff83bb6ab9c128816def55d17fccbeec5979032
bruno <bruno.silvaleite15@gmail.com>
2021-02-22 19:01:27 -0300

    utf-8 decoding

--- stream_consumer.py
+++ stream_consumer.py
@@ -7,7 +7,7 @@ def thingsboard(records):
     access_token = 'Put your Device Access Token here'
     for r in records:
         data = r['kinesis']['data']
-        decoded_data = base64.b64decode(data).decode('utf-8')
+        decoded_data = base64.b64decode(data).decode('iso-8859-1')
         requests.post(
             'https://thingsboard.cloud/api/v1/' + access_token + '/telemetry',
             data=decoded_data
