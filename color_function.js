f14deefb43baab293a4cb1c0c12751345cf0d5ed
bruno <bruno.silvaleite15@gmail.com>
2021-03-01 21:43:01 -0300

    Color function fix

--- color_function.js
+++ color_function.js
@@ -1,6 +1,9 @@
 var indice_calor = dsData[dsIndex]['INDICE_CALOR'];
-if (indice_calor <= 27) {
-    return "blue";
+if (indice_calor == "None") {
+    return "lightgrey"
+}
+else if (indice_calor <= 27) {
+    return "cyan";
 }
 else if (indice_calor <= 32) {
 	return "yellow";
